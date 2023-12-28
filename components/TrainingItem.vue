cae69c18bb7017393765c7e58de4d8e4e7e9e045
SerKo <serko.dev@gmail.com>
2023-12-29 01:10:06 +0800

    style: update frame style

--- components/TrainingItem.vue
+++ components/TrainingItem.vue
@@ -41,19 +41,19 @@ watch(totalScore, (score) => {
     <div class="flex gap-2 px-4 py-2 justify-between">
       <div class="flex items-center justify-center gap-2">
         <span class="text-sm text-green-700">基礎值</span>
-        <input v-model.number="inputBasicScore" class="input-score" type="number" placeholder="0">
+        <input v-model.number="inputBasicScore" class="input-score bg-green-200 text-green-900 placeholder:text-green-500" type="number" placeholder="0">
       </div>
 
       <div class="flex items-center justify-center gap-2">
-        <span class="text-sm text-green-700">已訓練</span>
-        <input v-model.number="inputTrainedScore" class="input-score" type="number" placeholder="0">
+        <span class="text-sm text-yellow-500">已訓練 +</span>
+        <input v-model.number="inputTrainedScore" class="input-score bg-yellow-100 text-yellow-700 placeholder:text-yellow-400" type="number" placeholder="0">
       </div>
     </div>
   </TrainingItemFrame>
 </template>
 
-<style>
+<style scoped>
 .input-score {
-  --at-apply: bg-green-300 input-number-hide-spin rounded text-center py-2 w-12 h-8 text-sm;
+  --at-apply:  input-number-hide-spin rounded text-center py-2 w-12 h-8 text-lg  font-semibold;
 }
 </style>
--- components/TrainingItemFrame.vue
+++ components/TrainingItemFrame.vue
@@ -15,7 +15,7 @@ const icon = [
 </script>
 
 <template>
-  <div class="of-hidden rounded-lg bg-green-100 ring-1 ring-green-200">
+  <div class="of-hidden rounded-lg bg-green-50 ring-1 ring-green-200">
     <div class="py-2 flex items-center justify-center gap-2 relative">
       <div :class="icon" class="text-xl text-green-700" />
       <div class="font-bold">

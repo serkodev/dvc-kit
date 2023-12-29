341e51efa6c0e6555e1d177cac7b0a37c718ca69
SerKo <serko.dev@gmail.com>
2023-12-29 23:52:26 +0800

    fix: checkbox label selection area

--- components/TheCheckbox.vue
+++ components/TheCheckbox.vue
@@ -7,8 +7,9 @@ const modelValue = defineModel()
 </script>
 
 <template>
-  <div class="flex items-center">
-    <input id="default-checkbox" v-model="modelValue" v-bind="$attrs" type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-    <label for="default-checkbox" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"><slot /></label>
-  </div>
+  <label class="flex items-center gap-1 text-sm font-medium text-gray-900 dark:text-gray-300">
+    <input v-model="modelValue" v-bind="$attrs" type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded">
+
+    <slot />
+  </label>
 </template>
